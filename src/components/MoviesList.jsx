1fa2d24925153372f029712d9ce475cdd9b7b1af
Josselin <julien.josselin75@hotmail.fr>
2023-10-30 15:44:11 +0100

    add axios + movies list + images

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,16 +1,18 @@
 import Screenshot from "../assets/Screenshot.png";
 
-const MoviesList = ({ movies }) => {
+const MoviesList = ({ url, movies }) => {
+  const BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+
   return (
     <div>
       <h2 className="special-title">My list</h2>
       <ul>
         {movies.map((movie, key) => (
           <li key={key}>
-            <p>{movie}</p>
+            <p>{movie.title}</p>
             <img
               className="movie-miniature"
-              src={Screenshot}
+              src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
               alt="movie illustration"
             />
           </li>
--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,26 +1,18 @@
 import { useEffect, useState } from "react";
 import MoviesList from "../components/MoviesList";
+import { getTrends } from "../services/tmdb";
+import axios from "axios";
 
 const Trending = () => {
-  const [click, setClick] = useState(0);
-  const moviesArray = ["Fast and furious", "Barbie", "Nemo"];
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    console.log("Trending");
-  }, [click]);
+    getTrends().then((movies) => {
+      setMovies(movies);
+    });
+  }, []);
 
-  const handleClick = () => {
-    console.log("Clicked");
-    setClick(click + 1);
-  };
-
-  return (
-    <>
-      <p>Likes : {click}</p>
-      <button onClick={handleClick}>Like</button>
-      <MoviesList movies={moviesArray} />
-    </>
-  );
+  return <MoviesList movies={movies} />;
 };
 
 export default Trending;
