ebabac4b41eefc4c14498d58a9e45567318504d9
Josselin <julien.josselin75@hotmail.fr>
2023-10-30 14:28:53 +0100

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import MoviesList from "./components/MoviesList";
 import SearchMovie from "./pages/SearchMovie";
 import Trending from "./pages/Trending";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <SearchMovie />
-      <Trending />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Trending />} />
+          <Route path="/search_movie" element={<SearchMovie />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
